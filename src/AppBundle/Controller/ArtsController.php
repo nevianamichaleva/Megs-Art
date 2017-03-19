b96716bee66decb81a72ceebb4a86fd878316931
nevianamichaleva <nevianamichaleva@gmail.com>
2017-03-20 00:00:36 +0200

    Control panel + send contact message

--- src/AppBundle/Controller/ArtsController.php
+++ src/AppBundle/Controller/ArtsController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use AppBundle\Entity\Arts;
 use AppBundle\Entity\Comments;
+use AppBundle\Form\ContactType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -43,18 +44,7 @@ class ArtsController extends Controller {
                     'lastSix' => $lastSix,
         ]);
     }
-
-    /**
-     * @Route("/contact", name="Contact us")
-     * @Route("/contact/{id}", name="Send buy message")
-     */
-    public function contactAction($id = null) {
-        // replace this example code with whatever you need
-        return $this->render('common/contact.html.twig', [
-                    'id' => $id
-        ]);
-    }
-
+    
     /**
      * @Route("/about", name="About")
      */
@@ -194,5 +184,65 @@ class ArtsController extends Controller {
                     'comments' => $comments,
         ));
     }
+    /**
+ * @Route("/contact", name="Contact us")
+ * @Route("/contact/{id}", name="Send buy message")
+ * 
+ */
+public function contactAction(Request $request)
+    {
+        // Create the form according to the FormType created previously.
+        // And give the proper parameters
+        $form = $this->createForm('AppBundle\Form\ContactType',null,array(
+            // To set the action use $this->generateUrl('route_identifier')
+            'action' => $this->generateUrl('Contact us'),
+            'method' => 'POST'
+        ));
 
+        if ($request->isMethod('POST')) {
+            // Refill the fields in case the form is not valid.
+            $form->handleRequest($request);
+
+            if($form->isValid()){
+                // Send mail
+                if($this->sendEmail($form->getData())){
+
+                    // Everything OK, redirect to wherever you want ! :
+                    
+                    return $this->redirectToRoute('Contact');
+                }else{
+                    // An error ocurred, handle
+                    var_dump("Errooooor :(");
+                }
+            }
+        }
+
+        return $this->render('contact/contacting.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
+
+    private function sendEmail($data){
+        $myappContactMail = '';
+        $myappContactPassword = '';
+        
+        // In this case we'll use the ZOHO mail services.
+        // If your service is another, then read the following article to know which smpt code to use and which port
+        // http://ourcodeworld.com/articles/read/14/swiftmailer-send-mails-from-php-easily-and-effortlessly
+        $transport = \Swift_SmtpTransport::newInstance('smtp.zoho.com', 465,'ssl')
+            ->setUsername($myappContactMail)
+            ->setPassword($myappContactPassword);
+
+        $mailer = \Swift_Mailer::newInstance($transport);
+        
+        $message = \Swift_Message::newInstance("Our Code World Contact Form ". $data["subject"])
+        ->setFrom(array($myappContactMail => "Message by ".$data["name"]))
+        ->setTo(array(
+            $myappContactMail => $myappContactMail
+        ))
+        ->setBody($data["message"]."<br>ContactMail :".$data["email"]);
+        
+        return $mailer->send($message);
+    }
 }
+
--- web/js/main.js
+++ web/js/main.js
@@ -1,20 +1,28 @@
- $(document).ready(function() {
-     $('.flexslider').flexslider({
-         animation: 'fade',
-         controlsContainer: '.flexslider'
-     });
-     $("#control-close").click(function(){
-         $(".control-panel").toggle("slide", { direction: "left" }, 1000);
-     });
-      $("#control-panel-close").click(function(){
-         $(".control-panel").toggle("slide", { direction: "left" }, 1000);
-     });
-     $(".control-panel a").click(function(ev){
-         imageUrl = '../images/'+ev.target.id+'.png';
-         $("body").css('background-image', 'url(' + imageUrl + ')');
-     });
-     $nav=$("#nav-main").html();
-     $("#nav-mobile").html($nav);
+$(document).ready(function () {
+ 
+    var currentColor = localStorage.getItem('background') ? localStorage.getItem('background') : '../images/body-bg.png';
+    
+    
+        $("body").css('background-image', 'url(' + currentColor + ')');
+    
+    
+    $('.flexslider').flexslider({
+        animation: 'fade',
+        controlsContainer: '.flexslider'
+    });
+    $("#control-close").click(function () {
+        $(".control-panel").toggle("slide", {direction: "left"}, 1000);
+    });
+    $("#control-panel-close").click(function () {
+        $(".control-panel").toggle("slide", {direction: "left"}, 1000);
+    });
+    $(".control-panel a.note-back").click(function (ev) {
+        imageUrl = '../images/' + ev.target.id + '.png';
+        localStorage.setItem('background', imageUrl);
+        $("body").css('background-image', 'url(' + imageUrl + ')');
+    });
+    $nav = $("#nav-main").html();
+    $("#nav-mobile").html($nav);
     $("#nav-trigger span").click(function () {
         if ($("nav#nav-mobile ul").hasClass("expanded")) {
             $("nav#nav-mobile ul.expanded").removeClass("expanded").slideUp(250);
@@ -35,10 +43,10 @@
         }
     });
     var $submenu = $("#nav-mobile li.submenu ul#sub-menu").html();
-    $("#nav-mobile li.submenu").hover(function(){
+    $("#nav-mobile li.submenu").hover(function () {
         var $lenght = ($("#nav-mobile ul li").size());
-        if ($lenght<=11){   
-        $("#nav-mobile ul").append($submenu);
-    }
-    });  
- });
+        if ($lenght <= 11) {
+            $("#nav-mobile ul").append($submenu);
+        }
+    });
+});
