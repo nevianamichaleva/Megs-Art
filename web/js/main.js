2cea67fbfa8c7b3b354cd6f5a0c32741e4b9e589
nevianamichaleva <nevianamichaleva@gmail.com>
2017-03-19 11:22:52 +0200

    Responsive + responsive menu

--- web/js/main.js
+++ web/js/main.js
@@ -3,4 +3,32 @@
          animation: 'fade',
          controlsContainer: '.flexslider'
      });
+     $nav=$("#nav-main").html();
+     $("#nav-mobile").html($nav);
+    $("#nav-trigger span").click(function () {
+        if ($("nav#nav-mobile ul").hasClass("expanded")) {
+            $("nav#nav-mobile ul.expanded").removeClass("expanded").slideUp(250);
+            $(this).removeClass("open");
+            $("#nav-trigger img").css({
+                "-webkit-transform": "rotate(360deg)",
+                "-moz-transform": "rotate(360deg)",
+                "transform": "rotate(360deg)"
+            });
+        } else {
+            $("nav#nav-mobile ul").addClass("expanded").slideDown(250);
+            $(this).addClass("open");
+            $("#nav-trigger img").css({
+                "-webkit-transform": "rotate(180deg)",
+                "-moz-transform": "rotate(180deg)",
+                "transform": "rotate(180deg)"
+            });
+        }
+    });
+    var $submenu = $("#nav-mobile li.submenu ul#sub-menu").html();
+    $("#nav-mobile li.submenu").hover(function(){
+        var $lenght = ($("#nav-mobile ul li").size());
+        if ($lenght<=11){   
+        $("#nav-mobile ul").append($submenu);
+    }
+    });  
  });
